74e818e5c5370a078070338cb812b4f74785096c
kalpitc19 <44117645+kalpitc19@users.noreply.github.com>
2018-10-14 14:35:45 +0530

    Add files via upload

---
+++ porn.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class pon
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a>0)
+System.out.println("positive");
+
+else if(a==0)
+System.out.println("zero");
+
+else
+System.out.println("negative");
+
+}
+}
